645012ba709fb5e74dc7b3a703d3d4298d145737
emiliano <emiliano@emiliano-VirtualBox.(none)>
2020-03-11 19:56:43 -0700

    passed some more tests

--- a7/LinkedList.c
+++ a7/LinkedList.c
@@ -246,7 +246,11 @@ int LLIterNext(LLIter iter) {
 
   // Step 7: if there is another node beyond the iterator, advance to it,
   // and return 0. If there isn't another node, return 1. 
-  
+  if (LLIterHasNext(iter)) {
+    iter->cur_node = iter->cur_node->next;
+    return 0;
+  }
+  return 1;
 }
 
 int LLIterGetPayload(LLIter iter, void** data) {
@@ -264,8 +268,12 @@ int LLIterHasPrev(LLIter iter) {
 int LLIterPrev(LLIter iter) {
   Assert007(iter != NULL); 
   // Step 8:  if there is another node beyond the iterator, go to it,
-  // and return 0. If not return 1. 
-
+  // and return 0. If not return 1.
+  if (LLIterHasPrev(iter)) {
+    iter->cur_node = iter->cur_node->prev;
+    return 0;
+  }
+  return 1;
 }
 
 int DestroyLLIter(LLIter iter) {
