32c15d99436fc17719d191c89030871efd52e670
emiliano <emiliano@emiliano-VirtualBox.(none)>
2020-03-18 16:39:43 -0700

    finished DestroyMovieSet function

--- a8/MovieSet.c
+++ a8/MovieSet.c
@@ -54,7 +54,7 @@ MovieSet CreateMovieSet(char *desc) {
 }
 
 void DestroyMovieSet(MovieSet set) {
-  // TODO(Student): What else to do to clean up a MovieSet?
-  // Free set
+  free(set->desc);
+  DestroyLinkedList(set->movies, &DestroyMovieWrapper);
   free(set);
 }
