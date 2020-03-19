497f3d2224dad19794766e7b8b077e7497f63ac4
emiliano <emiliano@emiliano-VirtualBox.(none)>
2020-03-18 23:56:15 -0700

    pulled starter code for a8

---
+++ a8/MovieIndex.c
@@ -0,0 +1,123 @@
+/*
+ *  Adrienne Slaughter
+ *  5007 Spr 2020
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  See <http://www.gnu.org/licenses/>.
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "MovieIndex.h"
+#include "htll/LinkedList.h"
+#include "htll/Hashtable.h"
+#include "Movie.h"
+#include "MovieSet.h"
+
+void DestroyMovieSetWrapper(void *movie_set) {
+  DestroyMovieSet((MovieSet)movie_set);
+}
+
+void toLower(char *str, int len) {
+  for (int i = 0; i < len; i++) {
+    str[i] = tolower(str[i]);
+  }
+}
+
+Index BuildMovieIndex(LinkedList movies, enum IndexField field_to_index) {
+  Index movie_index = CreateIndex();
+
+  // STEP 4(Student): Check that there is at least one movie
+  // What happens if there is not at least one movie?
+  LLIter iter = CreateLLIter(movies);
+  Movie* cur_movie;
+  LLIterGetPayload(iter, (void**)&cur_movie);
+
+  int result = AddMovieToIndex(movie_index, cur_movie, field_to_index);
+  
+  while (LLIterHasNext(iter)) {
+    LLIterNext(iter);
+    LLIterGetPayload(iter, (void**)&cur_movie);
+    result = AddMovieToIndex(movie_index, cur_movie, field_to_index);
+  }
+  DestroyLLIter(iter);
+  return movie_index;
+}
+
+
+int AddMovieActorsToIndex(Index index, Movie *movie) {
+  HTKeyValue kvp;
+  HTKeyValue old_kvp;
+
+  // STEP 6(Student): Add movies to the index via actors. 
+  //  Similar to STEP 5.
+
+  
+  AddMovieToSet((MovieSet)kvp.value, movie);
+}
+
+int AddMovieToIndex(Index index, Movie *movie, enum IndexField field) {
+  if (field == Actor) {
+    return AddMovieActorsToIndex(index, movie);
+  }
+
+  HTKeyValue kvp;
+  kvp.key = ComputeKey(movie, field, 0);
+  
+  // STEP 5(Student): How do we add movies to the index?
+  // The general idea: 
+  // Check hashtable to see if relevant MovieSet already exists
+  // If it does, grab access to it from the hashtable
+  // If it doesn't, create the new MovieSet and get the pointer to it
+  // Put the new MovieSet into the Hashtable.
+
+  // After we either created or retrieved the MovieSet from the Hashtable: 
+  AddMovieToSet((MovieSet)kvp.value, movie);
+
+  return 0;
+}
+
+uint64_t ComputeKey(Movie* movie, enum IndexField which_field, int which_actor) {
+  char rating_str[10];
+  switch (which_field) {
+    case Genre:
+      return FNVHash64((unsigned char*)movie->genre, strlen(movie->genre));
+    case StarRating:
+      snprintf(rating_str, 10, "%f", movie->star_rating);
+      return FNVHash64((unsigned char*)rating_str, strlen(rating_str));
+    case ContentRating:
+      return FNVHash64((unsigned char*)movie->content_rating,
+                       strlen(movie->content_rating));
+    case Actor:
+      if (which_actor < movie->num_actors) {
+        return FNVHash64((unsigned char*)movie->actor_list[which_actor],
+                         strlen(movie->actor_list[which_actor]));
+      }
+      break;
+  }
+  return -1u;
+}
+
+// Removed for simplicity
+//MovieSet GetMovieSet(Index index, const char *term){}
+
+int DestroyIndex(Index index) {
+  DestroyHashtable(index, &DestroyMovieSetWrapper);
+  return 0;
+}
+
+Index CreateIndex() {
+  return CreateHashtable(128);
+}
---
+++ a8/README.md
@@ -0,0 +1,34 @@
+### TODO Please edit the following information in this README
+
+- Name:
+- How many hours did it take you to complete this assignment?
+- Did you collaborate with any other students/TAs/Professors? (Name them below)
+  - tbd
+  - tbd
+- Did you use any external resources? (Cite them below)
+  - tbd
+  - tbd
+- (Optional) What was your favorite part of the assignment?
+- (Optional) How would you improve the assignment?
+
+See Canvas for all the details! 
+
+# A8: Building a Data Indexer
+
+## Files: 
+* ```FileParser```: Responsible for reading in a file, checking for errors, and parse out movies.
+* ```Movie```: A struct holding all the Movie info (id, title, type, genre, etc.)
+* ```MovieIndex```: A hashtable that indexes movies according to specified field. Basically wraps a hashtable with functionality specific to adding/removing/destroying with MovieSets. The key is the description for the document set, and the value is the document set.
+* ```MovieReport```: Responsible for printing out a report given a MovieIndex, to the command line or file.
+* ```MovieSet```: A set of Movies that are connected somehow: they have the same value for a given field. If the field is Year, all the movies in the set are made in the same year. It consists of a description and a LinkedList of Movies.
+
+
+## To run a single Google test
+
+Instead of running all the tests at once, you can run just a single test something like this: 
+
+```
+./test_fileparser --gtest_filter=FileParser.CreateMovieFromRow
+```
+
+Here, only the ```FileParser.CreateMovieFromRow``` test will run. 
